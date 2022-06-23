cddcf4ba653719fe5df64f7a4465bfaad88ee307
Jaka Maver <jmaver@plume.com>
2022-06-23 07:57:31 +0200

    Implement okta id token strategy

--- README.md
+++ README.md
@@ -1,15 +1,50 @@
-# typescript-starter-kit
+# passport-okta-id-token
 
-TypeScript starter kit for Node.js development.
-
-## Starting a new project
+## Install
 
 ```shell
-git clone https://github.com/jmaver-plume/typescript-starter-kit.git <directory-name>
-cd <directory-name>
-rm -rf .git
-git init
-git add .
-git commit -m "Initial commit"
-npm install
+npm install passport-okta-id-token
+```
+
+## Usage
+
+Strategy uses [@okta/jwt-verifier](https://www.npmjs.com/package/@okta/jwt-verifier/v/latest) under the hood.
+
+### Configure Strategy
+
+The strategy requires verifier options and verify callback. Both arguments are required.
+
+```javascript
+const { Strategy } = require("passport-okta-id-token");
+
+passport.use(
+  new Strategy({ issuer: "https://{yourOktaDomain}/oauth2/default" }, function (
+    jwt,
+    sub,
+    done
+  ) {
+    User.findOrCreate({ oktaId: sub }, function (err, user) {
+      return done(err, user);
+    });
+  })
+);
+```
+
+### Authenticate Requests
+
+```javascript
+app.post(
+  "/auth/okta",
+  passport.authenticate("okta-id-token", {
+    expectedClientId: "0oa5h93ocjgurRFu05d7",
+    expectedNonce:
+      "8V7Okhr8WqMGsXPQJaGy1Nu4GT5W05BfIbYlagBd587tryHKW6wWvQQiYvKFoZfJ",
+    getIdTokenFromRequest: (req) => req.body.id_token,
+    session: false,
+  }),
+  function (req, res) {
+    // do something with req.user
+    res.sendStatus(req.user ? 200 : 401);
+  }
+);
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,53 @@
-export function run(): string {
-  return "Hello World!";
+import { Strategy as PassportStrategy } from "passport-strategy";
+import OktaJwtVerifier, { VerifierOptions } from "@okta/jwt-verifier";
+import { Request } from "express";
+
+type VerifyFunction = (
+  jwt: OktaJwtVerifier.Jwt,
+  sub: string,
+  done: (err: Error, user: any, info: any) => void
+) => void;
+
+type AuthenticateOptions = {
+  expectedClientId: string;
+  expectedNonce: string;
+  getIdTokenFromReq: (req: Request) => string;
+};
+
+export class Strategy extends PassportStrategy {
+  private readonly name = "okta-id-token";
+  private readonly verify: VerifyFunction;
+  private readonly oktaJwtVerifier: OktaJwtVerifier;
+
+  constructor(options: VerifierOptions, verify: VerifyFunction) {
+    super();
+
+    this.verify = verify;
+    this.oktaJwtVerifier = new OktaJwtVerifier(options);
+  }
+
+  authenticate(req: Request, options: AuthenticateOptions) {
+    const token = options.getIdTokenFromReq(req);
+    if (!token) {
+      return this.fail({ message: "No ID token provided." }, 401);
+    }
+
+    const { expectedClientId, expectedNonce } = options;
+    this.oktaJwtVerifier
+      .verifyIdToken(token, expectedClientId, expectedNonce)
+      .then((jwt) => {
+        this.verify(jwt, jwt.claims.sub, (err, user, info) => {
+          if (err) {
+            return this.error(err);
+          }
+          if (!user) {
+            return this.fail(info);
+          }
+          return this.success(user, info);
+        });
+      })
+      .catch((err) => {
+        this.fail({ message: err.message }, 401);
+      });
+  }
 }
